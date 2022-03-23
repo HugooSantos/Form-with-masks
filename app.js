e9a7d877db9d36774850b4d5c5e4bdaa28bac0d4
HugooSantos <hugogabrieltjj@gmail.com>
2022-03-22 22:11:50 -0300

    did masks in cnpj input

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const masks = {
 
     cnpj(value){
         return value.replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/,'$1.$2')
+        .replace(/(\d{3})(\d)/,'$1.$2')
+        .replace(/(\d{3})(\d)/,'$1/$2')                
+        .replace(/(\d{4})(\d)/,'$1-$2')
+        .replace(/(-\d{2})\d+?$/,'$1')          
     }
 }
 

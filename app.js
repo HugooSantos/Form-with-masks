5455931c1f2efbb1cb5d6c5bc5fa0150e7f2af9a
HugooSantos <hugogabrieltjj@gmail.com>
2022-03-23 07:04:46 -0300

    finish mask phone

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ const masks = {
         .replace(/(\d{3})(\d)/,'$1/$2')                
         .replace(/(\d{4})(\d)/,'$1-$2')
         .replace(/(-\d{2})\d+?$/,'$1')          
+    },
+    phone(value){
+        return value.replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4,5})\d+?$/,'$1')    
     }
+    
 }
 
 

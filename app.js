eb46dd5a28a524b54bb3142b71327ed1f7998559
HugooSantos <hugogabrieltjj@gmail.com>
2022-03-24 20:31:01 -0300

    cep done

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ const masks = {
         .replace(/(\d{4})(\d)/, '$1-$2')
         .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
         .replace(/(-\d{4,5})\d+?$/,'$1')    
+    },
+    cep(value){
+        return value.replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/,'$1')  
+    },
+    pis(value){
+        
     }
-    
 }
 
 

009b19887459c7f73d794a8bde3cd39702f9cf06
HugooSantos <hugogabrieltjj@gmail.com>
2022-03-24 21:13:30 -0300

    add PIS

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ const masks = {
         .replace(/(-\d{3})\d+?$/,'$1')  
     },
     pis(value){
-        
+        return value.replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/,'$1.$2')
+        .replace(/(\d{5})(\d)/,'$1.$2')
+        .replace(/(\d{5}\.)(\d{2})(\d)/,'$1$2-$3')
+        .replace(/(-\d)\d+?$/,'$1')  
     }
 }
 
